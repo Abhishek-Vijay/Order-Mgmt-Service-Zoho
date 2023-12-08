ddab3af0070141021eddd6f267f03043c3f9a4f8
ChinmayaDatta <chinmaya.datta@tatamd.com>
2023-12-08 15:28:30 +0530

    APIS for products and plans

--- helper/envHelper.js
+++ helper/envHelper.js
@@ -12,7 +12,7 @@ const envVariables = {
     // BILLING_CLIENT_ID:get(env, 'BILLING_CLIENT_ID') || '1000.X84CRC9OL870OQM12BW455QY6OLCAO',
     // BILLING_CLIENT_SECRET:get(env, 'BILLING_CLIENT_SECRET') || '917b24a38df1f4f5ebaa58c133496122ae4346ad71',
     // BILLING_REFRESH_TOKEN:get(env, 'BILLING_REFRESH_TOKEN') || '1000.72abeb2bce9b500732d94e006110056c.6759989339b9ca7e5624d63f6cfd6805',
-    BILLING_ORGANIZATION_ID:get(env, 'BILLING_ORGANIZATION_ID') || '60025084079',
+  //  BILLING_ORGANIZATION_ID:get(env, 'BILLING_ORGANIZATION_ID') || '60025084079',
     REGION: get(env, 'REGION') || 'ap-south-1',
     S3_BUCKET: get(env, 'S3_BUCKET') || 'urban-care-documents',
     S3_FOLDER: get(env, 'S3_FOLDER') || 'patient-invoice',
@@ -28,8 +28,8 @@ const envVariables = {
         POSTGRES_PORT: get(env, 'POSTGRES_PORT') || 5432
     },
 
-    // changed combined scope to "ZohoBooks.invoices.CREATE,ZohoBooks.invoices.READ,ZohoBooks.invoices.UPDATE,ZohoBooks.invoices.DELETE,ZohoBooks.contacts.CREATE,ZohoBooks.contacts.UPDATE,ZohoBooks.contacts.READ,ZohoBooks.contacts.DELETE,ZohoBooks.settings.READ,ZohoSubscriptions.customers.CREATE,ZohoSubscriptions.customers.UPDATE,ZohoSubscriptions.customers.READ,ZohoSubscriptions.customers.DELETE,ZohoSubscriptions.subscriptions.CREATE,ZohoSubscriptions.subscriptions.UPDATE,ZohoSubscriptions.subscriptions.READ,ZohoSubscriptions.subscriptions.DELETE,ZohoSubscriptions.invoices.CREATE,ZohoSubscriptions.invoices.UPDATE,ZohoSubscriptions.invoices.READ,ZohoSubscriptions.invoices.DELETE,ZohoSubscriptions.plans.CREATE,ZohoSubscriptions.plans.UPDATE,ZohoSubscriptions.plans.READ,ZohoSubscriptions.plans.DELETE,ZohoSubscriptions.hostedpages.CREATE,ZohoSubscriptions.hostedpages.READ,ZohoSubscriptions.payments.CREATE,ZohoSubscriptions.payments.UPDATE,ZohoSubscriptions.payments.READ,ZohoSubscriptions.payments.DELETE,ZohoSubscriptions.settings.READ"
-    REFRESH_TOKEN: get(env, 'REFRESH_TOKEN') || '1000.81d55392704a32aa190ae39975648c90.46ec1140c056fa1b5a797c1a1fc4d163',
+    // changed combined scope to "ZohoSubscriptions.products.CREATE,ZohoSubscriptions.products.UPDATE,ZohoSubscriptions.products.READ,ZohoSubscriptions.products.DELETE,ZohoBooks.invoices.CREATE,ZohoBooks.invoices.READ,ZohoBooks.invoices.UPDATE,ZohoBooks.invoices.DELETE,ZohoBooks.contacts.CREATE,ZohoBooks.contacts.UPDATE,ZohoBooks.contacts.READ,ZohoBooks.contacts.DELETE,ZohoBooks.settings.READ,ZohoSubscriptions.customers.CREATE,ZohoSubscriptions.customers.UPDATE,ZohoSubscriptions.customers.READ,ZohoSubscriptions.customers.DELETE,ZohoSubscriptions.subscriptions.CREATE,ZohoSubscriptions.subscriptions.UPDATE,ZohoSubscriptions.subscriptions.READ,ZohoSubscriptions.subscriptions.DELETE,ZohoSubscriptions.invoices.CREATE,ZohoSubscriptions.invoices.UPDATE,ZohoSubscriptions.invoices.READ,ZohoSubscriptions.invoices.DELETE,ZohoSubscriptions.plans.CREATE,ZohoSubscriptions.plans.UPDATE,ZohoSubscriptions.plans.READ,ZohoSubscriptions.plans.DELETE,ZohoSubscriptions.hostedpages.CREATE,ZohoSubscriptions.hostedpages.READ,ZohoSubscriptions.payments.CREATE,ZohoSubscriptions.payments.UPDATE,ZohoSubscriptions.payments.READ,ZohoSubscriptions.payments.DELETE,ZohoSubscriptions.settings.READ"
+    REFRESH_TOKEN: get(env, 'REFRESH_TOKEN') || '1000.8d36c76cfbae80064330c218823720af.ae3577c4827947f3b6013ee9aba83cf0',
     APP_SERVER_PORT: get(env, 'APP_SERVER_PORT') || 5000
 }
 
